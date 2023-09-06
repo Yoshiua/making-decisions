09a454fdfa5ea91f02f046e925349c7c340bbf20
Josh Olson <YoshiOlson@yahoo.com>
2023-09-06 17:43:37 -0600

    finishedMakingDecisions

--- lab.js
+++ lab.js
@@ -10,7 +10,7 @@
 
 let lovesCode = true;
 
-if (lovesCode = true) {
+if (lovesCode === true) {
 console.log('I love to code!')
 } else {
   console.log('Coding has its challenges')
@@ -22,7 +22,7 @@ console.log('I love to code!')
 var amysAge = 29;
 var brittanisAge = 34;
 var amysBirthYear = 1991;
-var brittanisBirthYear = 1986;
+var brittanisBirthYear = 1984;
 
 ////////// PROBLEM 2 //////////
 
@@ -32,13 +32,13 @@ var brittanisBirthYear = 1986;
 
 //CODE HERE
 
-// if (amysAge > brittanisAge) { 
-// console.log ("Amy is older")
-// }    else if (amysAge === brittanisAge) { 
-// console.log("Amy and Brittani are the same age")
-// }   else { 
-// console.log("Brittani Is older")
-// }
+if (amysAge > brittanisAge) { 
+console.log ("Amy is older")
+}    else if (amysAge === brittanisAge) { 
+console.log("Amy and Brittani are the same age")
+}   else { 
+console.log("Brittani Is older")
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -48,16 +48,16 @@ var brittanisBirthYear = 1986;
 
 //CODE HERE
 
-// if (amysBirthYear === brittanisBirthYear) {
-//   console.log ("Amy & Brittani were born in the same year")
-// } else {
-//   console.log ("Amy & Brittani were not born in the same year")
-// }
+if (amysBirthYear === brittanisBirthYear) {
+  console.log ("Amy & Brittani were born in the same year")
+} else {
+  console.log ("Amy & Brittani were not born in the same year")
+}
 
 ////////// PROBLEM 4 //////////
 
-let temperature = 55;
-let rain = true;
+let temperature = 66;
+let rain = false;
 
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
@@ -67,17 +67,17 @@ let rain = true;
 
 //CODE HERE
 
-// if (temperature >= 80 && rain = true) {
-//   console.log(" wear a t-shirt and take an umbrella")
-// }   else if (temperature >= 60 && temperature<=80 && rain = true) {
-//   console.log("wear a rain-jacket") 
-// }   else if (temperature <=60) {
-//   console.log("wear a jacket and carry an umbrella")
-// }   else if (temperature >= 80 && rain = false) {
-//   console.log(" wear a t-shirt")
-// }   else if (temperature >= 60 && temperature<=80 && rain = false) {
-//   console.log("wear long sleeve")
-// }
+if (temperature >= 80 && rain === true) {
+  console.log(" wear a t-shirt and take an umbrella")
+}   else if (temperature >= 60 && temperature<=80 && rain === true) {
+  console.log("wear a rain-jacket") 
+}   else if (temperature <=60) {
+  console.log("wear a jacket and carry an umbrella")
+}   else if (temperature >= 80 && rain === false) {
+  console.log(" wear a t-shirt")
+}   else if (temperature >= 60 && temperature<=80 && rain === false) {
+  console.log("wear long sleeve")
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -87,9 +87,9 @@ let rain = true;
 
 //CODE HERE
 
-// for (let i=0; i<10; i++){
-//   console.log('hello')
-// }
+for (let i=0; i<10; i++){
+  console.log(i)
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -99,9 +99,9 @@ let rain = true;
 
 //CODE HERE
 
-// for (let i=0; i<10; i++){
-//   console.log('$i')
-// }
+for (let i=0; i<10; i++){
+  console.log("")
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -111,14 +111,14 @@ let rain = true;
 
 //CODE HERE
 
-// for (let i=10; i>0; i--){
-//   console.log('$i')
-// }
+for (let i=10; i>0; i--){
+  console.log(i)
+}
 
 ////////// PROBLEM 8 //////////
 
-// let score = 0;
-// let passingScore = 7;
+let score = 0;
+let passingScore = 7;
 
 /*
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, 
@@ -127,10 +127,10 @@ let rain = true;
 
 //CODE HERE
 
-// while (score >7){
-//  console.log("your score is not high enough")
-//  score++
-// }
+while (score < passingScore){
+  console.log('Your score is not high enough')
+  score++
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
@@ -143,26 +143,56 @@ let rain = true;
 
 //CODE HERE
 
-// let changeMyMind = true
+let changeMyMind = true
 
-// if (changeMyMind = true){
-//   changeMyMind =false
-// }
-
-// if (changeMyMind = false){
-//   changeMyMind =true
-// }
+if (changeMyMind === true){
+  changeMyMind = false
+  console.log (changeMyMind)
+}   else {
+  changeMyMind = true
+  console.log (changeMyMind)
+}
 
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, 
+// console.log it's new value (it should now read true).
 
 //CODE HERE
+
+if (!changeMyMind){
+  changeMyMind = true
+  console.log(changeMyMind)
+}
 
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
 
 let z = 5;
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. 
+//If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+
+
+while (z >0){
+  let countdown =z;
+  while (countdown > 0){
+    console.log(countdown);
+    countdown--;
+  }
+z--;
+}
+
+
+// let countdown = z;
+
+
+//   while (countdown > 0){
+//     console.log(countdown)
+//     countdown--
+//   }
+//     while (z > 1){
+//       z--
+//       console.log(z)
+//     }
